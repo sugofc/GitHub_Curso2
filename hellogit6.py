3bfa80aa5c5fc0f7025ab65104910d553715881c
sugo <fhcp87@gmail.com>
2023-06-02 10:32:48 -0400

    comentario largo pr

--- hellogit6.py
+++ hellogit6.py
@@ -1,3 +1,9 @@
 print("nuevo archivo git")
 print("agregando una 2da linea de prueba")
 print("generar conflicto")
+
+"""
+Aqui va un comentario largo
+para probar como se comporta
+al crear un pull request.
+"""
